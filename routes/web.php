02ab4e86917b8774b90e0bb0d6d047a19f7c3217
BINTANG RIZQI PASHA <87906435+BintangRP@users.noreply.github.com>
2024-05-25 17:15:23 +0700

    feat(routes): web routes

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,33 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProjectController;
+use App\Http\Middleware\Authenticate;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [ProjectController::class, 'index'])->name('home');
-Route::get('/project/{id}', [ProjectController::class, 'show'])->name('project.show');
+// route landing page
+Route::get('/', [ProjectController::class, 'landingPage'])->name('landingPage');
 
-Route::get('/login', [ProjectController::class, 'index'])->name('auth');
+// 404 content
+Route::fallback(function () {
+    return view('404');
+});
 
-Route::middleware(['auth'])->group(function () {
+// route projects
+Route::get('/projects', [ProjectController::class, 'projects'])->name('projects');
+Route::get('/projects/{id}', [ProjectController::class, 'show'])->name('project.show');
+
+// route login logout
+Route::get('/login', [AuthController::class, 'index'])->name('auth');
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+
+// route admin + CRUD project
+Route::middleware([Authenticate::class])->group(function () {
     Route::get('/admin', [ProjectController::class, 'admin'])->name('admin');
-    Route::post('/admin/project', [ProjectController::class, 'store'])->name('project.store');
-    Route::put('/admin/project/{id}', [ProjectController::class, 'update'])->name('project.update');
-    Route::delete('/admin/project/{id}', [ProjectController::class, 'destroy'])->name('project.destroy');
+    Route::get('/admin/projects', [ProjectController::class, 'create'])->name('project.create');
+    Route::post('/admin/projects', [ProjectController::class, 'store'])->name('project.store');
+    Route::get('/admin/projects/{id}', [ProjectController::class, 'edit'])->name('project.edit');
+    Route::patch('/admin/projects/{id}', [ProjectController::class, 'update'])->name('project.update');
+    Route::delete('/admin/projects/{id}', [ProjectController::class, 'destroy'])->name('project.destroy');
+    Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
 });
