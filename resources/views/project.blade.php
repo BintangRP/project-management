4fa09efe0636df1877a26ce03464e2fb08d6a72a
BINTANG RIZQI PASHA <87906435+BintangRP@users.noreply.github.com>
2024-05-25 17:16:23 +0700

    feat(projects): projects view

--- resources/views/project.blade.php
+++ resources/views/project.blade.php
@@ -1,3 +1,14 @@
-<div>
-    <!-- Nothing worth having comes easy. - Theodore Roosevelt -->
-</div>
+@extends('layouts.index')
+
+@section('title', 'Landing Page')
+
+@section('content')
+    <div class="container-lg">
+        <h1>{{ $project->name }}</h1>
+        <a href="{{ $project->link_project }}">Link to Project</a>
+        <img class="d-flex justify-content-center mx-auto" src="{{ asset($project->image) }}" alt="{{ $project->name }}"
+            height="300">
+        <h2>Deksripsi:</h2>
+        <p>{{ $project->description }}</p>
+    </div>
+@endsection
--- resources/views/projects.blade.php
+++ resources/views/projects.blade.php
@@ -1,3 +1,27 @@
-<div>
-    <!-- Simplicity is the essence of happiness. - Cedric Bledsoe -->
-</div>
+@extends('layouts.index')
+
+@section('title', 'Landing Page')
+
+@section('content')
+    <link rel="stylesheet" href="{{ asset('styles/gallery.css') }}">
+    <div class="container-lg">
+        <h2 class="mt-5">All Projects</h2>
+        <div class="gallery">
+            @foreach ($projects as $project)
+                <div class="gallery-item">
+                    <img src="{{ asset($project->image) }}" alt="{{ $project->name }}" class="gallery-image">
+                    <div class="overlay">
+                        <div class="text">
+                            <h3>{{ $project->name }}</h3>
+                            <p>{{ Str::limit($project->description, 30) }}</p>
+                            <p>Stack: {{ $project->stack }}</p>
+                            <p>Link: <a href="{{ $project->link_project }}" target="_blank">Halaman Project</a>
+                            </p>
+                            <a href="{{ route('project.show', $project->id) }}" class="btn btn-primary">Detail</a>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
+@endsection
