498c570820f58c5608f295b029cc561fbffbc010
BINTANG RIZQI PASHA <87906435+BintangRP@users.noreply.github.com>
2024-05-25 17:16:11 +0700

    feat(auth): login view

--- resources/views/auth.blade.php
+++ resources/views/auth.blade.php
@@ -1,16 +1,20 @@
-@extends('layouts.app')
+@extends('layouts.index')
 
 @section('content')
     <div class="container">
         <h1>Login Admin</h1>
 
-        @if ($errors->any())
-            <div class="alert alert-danger">
-                <ul>
-                    @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
+        @if (session('success'))
+            <div class="alert alert-success alert-dismissible fade show" role="alert">
+                <strong>{{ session('success') }}</strong>
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+        @endif
+
+        @if (session('error'))
+            <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                <strong>{{ session('error') }}</strong>
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
             </div>
         @endif
 
@@ -19,8 +23,8 @@
             @method('POST')
             <div class="form-group">
                 <label for="email">Email</label>
-                <input type="text" class="form-control @error('message') is-invalid @enderror" id="email" name="email"
-                    required>
+                <input type="text" class="form-control @error('message') is-invalid @enderror" id="email"
+                    name="email" required>
             </div>
             <div class="form-group">
                 <label for="password">Password</label>
